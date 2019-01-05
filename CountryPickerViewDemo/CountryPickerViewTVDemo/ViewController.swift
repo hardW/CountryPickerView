ef66472f5869008c3de8655ea8de8cd2ac970d77
Maxim Bunkov <bimawa@gmail.com>
2019-01-05 09:47:48 +0500

    Added tvos support. Reorganize coe

---
+++ CountryPickerViewDemo/CountryPickerViewTVDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryPickerViewTVDemo
+//
+//  Created by Maxim Bunkov on 05/01/2019.
+//  Copyright © 2019 Kizito Nwose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
